136f7757c6e02b69ac3f7548f4654856766755b6
Khoa D. Vo <khoa.synthesis@gmail.com>
2022-09-29 12:08:08 +0700

    add AHIQ inference

---
+++ inference_ahiq.py
@@ -0,0 +1,53 @@
+import argparse
+import glob
+import os
+from pyiqa import create_metric
+from tqdm import tqdm
+
+
+def main():
+    """Inference demo for pyiqa.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str, default=None, help='input image/folder path.')
+    parser.add_argument('-r', '--ref', type=str, default=None, help='reference image/folder path if needed.')
+
+    args = parser.parse_args()
+
+    metric_name = 'AHIQ'.lower()
+
+    # set up IQA model
+    iqa_model = create_metric(metric_name, metric_mode='FR')
+
+    if os.path.isfile(args.input):
+        input_paths = [args.input]
+        if args.ref is not None:
+            ref_paths = [args.ref]
+    else:
+        input_paths = sorted(glob.glob(os.path.join(args.input, '*')))
+        if args.ref is not None:
+            ref_paths = sorted(glob.glob(os.path.join(args.ref, '*')))
+
+    total_avg = 0
+    n_iter = 100
+
+    for i in range(n_iter):
+        avg_score = 0
+        test_img_num = len(input_paths)
+        pbar = tqdm(total=test_img_num, unit='image')
+        for idx, img_path in enumerate(input_paths):
+            ref_img_path = ref_paths[idx]
+
+            score = iqa_model(img_path, ref_img_path).cpu().item()
+            avg_score += score
+            pbar.update(1)
+        pbar.close()
+        avg_score /= test_img_num
+        total_avg += avg_score
+        print(f'AHIQ[{i}]: {round(avg_score, 4)}')
+
+    print(f'Average AHIQ: {round(total_avg / n_iter, 4)}')
+
+
+if __name__ == '__main__':
+    main()
