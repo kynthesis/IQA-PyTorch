c9da25cf46f27380545770fc5260f1e20b22c7ad
Khoa D. Vo <khoa.synthesis@gmail.com>
2022-10-17 10:09:06 +0700

    add LPIPS, NIQE, PSNR, SSIM customized inference

--- inference_lpips.py
+++ inference_lpips.py
@@ -10,29 +10,37 @@ def main():
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', type=str, default=None, help='input image/folder path.')
+    parser.add_argument('-r', '--ref', type=str, default=None, help='reference image/folder path if needed.')
 
     args = parser.parse_args()
 
-    metric_name = 'NIQE'.lower()
+    metric_name = 'LPIPS'.lower()
 
     # set up IQA model
-    iqa_model = create_metric(metric_name, metric_mode='NR')
+    iqa_model = create_metric(metric_name, metric_mode='FR')
 
     if os.path.isfile(args.input):
         input_paths = [args.input]
+        if args.ref is not None:
+            ref_paths = [args.ref]
     else:
         input_paths = sorted(glob.glob(os.path.join(args.input, '*')))
+        if args.ref is not None:
+            ref_paths = sorted(glob.glob(os.path.join(args.ref, '*')))
+
 
     avg_score = 0
     test_img_num = len(input_paths)
     pbar = tqdm(total=test_img_num, unit='image')
-    for img_path in input_paths:
-        score = iqa_model(img_path).cpu().item()
+    for idx, img_path in enumerate(input_paths):
+        ref_img_path = ref_paths[idx]
+
+        score = iqa_model(img_path, ref_img_path).cpu().item()
         avg_score += score
         pbar.update(1)
     pbar.close()
     avg_score /= test_img_num
-    print(f'NIQE: {round(avg_score, 4)}')
+    print(f'LPIPS: {round(avg_score, 4)}')
 
 
 if __name__ == '__main__':
